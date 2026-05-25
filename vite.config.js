f26e758a6c0453762226a8042a5c46df23f16d97
would1920 <would1920@gmail.com>
2026-05-25 12:09:26 +0800

    Use gh-pages deployment config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Hackson/' : '/',
   plugins: [react()],
   base: '/zgca-hackathon/',
   server: {
